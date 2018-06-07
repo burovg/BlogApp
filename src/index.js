e9494f5c925f8a0992cfa7e02361cebfe7e0fafc
burovg <burovg@gmail.com>
2018-06-07 16:23:12 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -6,10 +6,33 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends Component{
+  render(){
+    return(
+      <div>Hello!</div>
+    );
+  }
+}
+
+class GoogBye extends Component{
+  render(){
+    return(
+      <div>Good Bye !</div>
+    );
+  }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+      <Route path='/hello' component={Hello} />
+      <Route path='/goodbye' component={GoogBye} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
